85780af52e722c63ea8462ddbe60da255a158df5
stephenwhite <stephen.white@callistaenterprise.se>
2015-10-04 07:37:37 +0200

    1. postId in state, init post in the connect method, now also on comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -7,43 +7,20 @@ import {utils} from '../utils';
 export const Comments = React.createClass({
   mixins: [React.addons.PureRenderMixin],
 
-  comments : [],
-
-  getComments(props){
-    console.log('+++++++++');
-    let postId = parseInt(props.params.postId);
-    let {state} = props;
-    return utils.filterList(state, 'comments', 'post', postId);
-  },
-
-  // called on the initial mounting and only once.
-  componentWillMount: function() {
-    console.log('------------');
-    console.log('comments, componentWillMount');
-    this.comments = this.getComments(this.props);
-  },
-
-  // only called when props and state are being updated
-  componentWillReceiveProps: function(nextProps) {
-    console.log('------------');
-    console.log('comments, componentWillReceiveProps');
-    this.comments = this.getComments(nextProps);
-  },
-
   addAComment(event){
     if (event.which === 13) {
       var comment = event.target.value;
-      let {addComment, state} = this.props;
+      let {addComment} = this.props;
 
       addComment(comment, parseInt(this.props.params.postId));
     }
   },
 
   render: function() {
-
+    let {comments} = this.props;
     let commentsMu = <li>No Comments!</li>
-    if(this.comments != null) {
-      commentsMu = this.comments.map((comment)=> {
+    if(comments != null) {
+      commentsMu = comments.map((comment)=> {
         let id = comment.get('_id');
         let commentsLink = `comments/${id}`;
         return <li key={comment.get('_id')}><span>{comment.get('date')}</span> <span>{comment.get('body')}</span> </li>
@@ -66,7 +43,7 @@ function mapStateToProps(state) {
   //console.log('---- Comments : ' + JSON.stringify(state, null, 2));
   return {
     state: state,
-    comments: state.get('currentComments')
+    comments: utils.filterList(state, 'comments', 'post', state.get('currentPost'))
   };
 }
 
